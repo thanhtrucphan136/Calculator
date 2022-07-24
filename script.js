e32f8727a985decb294013ea439d3ca8df47ceba
Truc Phan <trucphan@my.unt.edu>
2022-07-24 14:03:23 -0500

    create 4 basic math operators

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+console.log(add(20,1));
+console.log(subtract(10,5));
+console.log(multiply(4,9));
+console.log(divide(10,3));
