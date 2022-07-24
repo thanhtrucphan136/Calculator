b13c0dc050765371f6148833d74ad05d8656fd11
Truc Phan <trucphan@my.unt.edu>
2022-07-24 15:04:45 -0500

    make number appear when clicked and clear the display when clicked AC

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+
+const displayScreen = document.getElementById('display');
+const numpad = document.getElementById('numpad');
+let nums = numpad.querySelectorAll('input');
+let displayStorage = '';
+nums.forEach((num) => 
+    num.addEventListener('click', () => display(num.value))
+);
+
+function display(number){
+    if (number == 'AC'){
+        clear();
+    }else{
+        displayScreen.textContent = number;
+        displayStorage += number;
+        console.log(displayStorage);
+    }
+}
+
+function clear(){
+    displayScreen.textContent = '';
+    displayStorage = '';
+    //displayStorage = displayStorage.slice(0,-1);
+}
+
 function add(a,b){
     return a + b;
 }
@@ -7,14 +32,15 @@ function subtract(a,b){
 }
 
 function multiply(a,b){
-    return a*b;
+    return a * b;
 }
 
 function divide(a,b){
-    return a/b;
+    return a / b;
 }
 
+/*
 console.log(add(20,1));
 console.log(subtract(10,5));
 console.log(multiply(4,9));
-console.log(divide(10,3));
+console.log(divide(10,3));*/
